3fe4c1403d6e11f941cb0891e1509d5291ec7d19
TobiOnabolu <toby.onabolu@gmail.com>
2021-05-15 16:50:39 -0400

    Small Changes

--- safetyapp/src/App.js
+++ safetyapp/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './App.css';   //the ./ mean current directory
+//we assign the name number to this file path
+import navbar from './components/navbar.js';  //only importing js cause js imports cs; importing so we can use in our app js file
 
 function App() {
   return (
     <div className="App">
+      <navbar/>
       <p>Hello</p>
     </div>
   );
---
+++ safetyapp/src/components/navbar.js
@@ -0,0 +1,12 @@
+import './navbar.css';
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default navbar
