a82ff46e5b616ea490efe3a407a22a07d85efa07
kendzisah <dzisahken10@gmail.com>
2021-05-16 17:23:38 -0400

    Finised creating the welcome page, just some styling left

--- safetyapp/src/App.js
+++ safetyapp/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Welcomepage from './components/pages/Welcomepage';
+
 
 
 function App() {
@@ -7,7 +9,8 @@ function App() {
   return (
     <div className="App">
       
-
+      <Welcomepage />
+      
     </div>
   );
 }
--- safetyapp/src/components/pages/welcomepage.js
+++ safetyapp/src/components/pages/welcomepage.js
@@ -1,12 +1,35 @@
-import './welcomepage.css';
-import React from 'react'
+import './Welcomepage.css';
+import React from 'react';
+import { Button } from 'reactstrap';
 
-function mainpage() {
+
+function Welcomepage() {
     return (
-        <div>
-            
+        <div className="container-1">
+            <div className="header-logo">
+                <i class="fas fa-peace"> SAFETY APP</i>
+            </div>
+
+            <section className="statement left" >
+                <h3>HOW TO PROTECT YOURSELF AND FAMILY</h3>
+                <p>Learning about a city or area ahead of time will allow
+                 you to make well guided decisions on whether you 
+                should travel or enter an area ahead of time. 
+                Staying informed is a key skill to maintaining the 
+                safety and happiness of your family and friends.</p>
+            </section>
+
+            <section className="statement right" >
+                <h3>SAFETY, FREEDOM, PEACE, LONGEVITY</h3>
+                <p>We aim to provide people with the means and
+                 tools to enter a state of being free from danger
+                  or threat.</p>
+            </section>
+
+            <img src="./images/temp_skyline.svg" alt="A city skyline." className="skyline-img" />
+            <Button  className="start-btn" size="lg">START NOW</Button>
         </div>
     )
 }
 
-export default mainpage;
+export default Welcomepage;
