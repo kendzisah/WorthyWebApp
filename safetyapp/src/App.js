66e925f61e6354a5bbdc34c010569baccf6a3018
TobiOnabolu <toby.onabolu@gmail.com>
2021-05-15 21:50:41 -0400

    V1 w/ broken images

--- safetyapp/src/App.js
+++ safetyapp/src/App.js
@@ -1,14 +1,24 @@
-import './App.css';   //the ./ mean current directory
-//we assign the name number to this file path
-import navbar from './components/navbar.js';  //only importing js cause js imports cs; importing so we can use in our app js file
+import './App.css';   
+import Navbar from './Components/Navbar/Navbar.js';  
 
 function App() {
   return (
     <div className="App">
-      <navbar/>
-      <p>Hello</p>
+      <Navbar/>
     </div>
   );
 }
+
+<div>{
+/*
+Quick things I picked up
+- the ./ mean current directory
+- only importing js cause js imports cs; importing so we can use in our app js file
+
+Questions
+
+*/
+}</div>
+
 
 export default App;
--- safetyapp/src/Components/Navbar/Navbar.js
+++ safetyapp/src/Components/Navbar/Navbar.js
@@ -1,62 +1,70 @@
-import './navbar.css';
-//imrc is shortcut to type this react statement
+import './Navbar.css';
 import React, { Component } from 'react'
 import { MenuItems } from './MenuItem.js'
 
-
-//this was creating a navbar using function
-//jsx format for doing a function
-//return value is navbar
-/*
-const navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-*/
-
-//creating a navbar using class method
-//since we are doing it the class way needs to derive component class
 class Navbar extends Component 
 {        
-    //always need a render Q: WHATS A RENDER
     render() 
     {
-        //return something Q: DONT KNOW WHAT YET
-        return  
-        (
-            //dont get this confused with nav bar, this is a tag for navigation links
-            //also to give a container a variable name we use className; see how nav var name is navbar items, and beloow that the h1 var name is navbar logo
-            //notice nav link is our top most container in this class
+        return  (            
             <nav className="NavbarItems">
-                //Q: DONT KNOW WHY WERE PUTTING REACT MIGHT HAVE TO CHANGE THAT TO AN IMAGE OF OURS
-                <h1 className="Navbar-logo">React</h1>
-                <div className="Menu-Icon">
-                    //In this div we will have something later 
-                </div>
-                // ul tag is how we create an array of unordered elements, we then populate the ul with li tags(list items)
-                <ul>
-                    //a tag is to insert a hyper link
-                   <li><a href=""></a></li> 
+                <div>{/*Populating navbar*/}</div>
+                <ul class="Items">
+                    {   
+                        MenuItems.map
+                        (
+                            (item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <a className={item.cName} href={item.url}>
+                                            <img src={item.icon} alt="" width="40" height="40"></img>
+                                        </a>
+                                        <div>{/*Needed another 'a' tag to be able to add the title underneath the image; would be better if I can add these to one hyper link with words at bottom*/}</div>
+                                        <a className="Links" href={item.url}>
+                                            <h5>{item.title}</h5>
+                                        </a>
+                                    </li> 
+                                )
+                            }
+                        )
+                    } 
                 </ul>
-
-
             </nav>                   
-            
-
         )
 
     }
 }
 
-//this was for if we did it the function way
-//export default navbar
-
+<div>{
 /*
+<img alt="Qries" src="https://www.qries.com/images/banner_logo.png"
+         width=150" height="70">
 Quick things I picked up
+- We created the navbar using class method, could have also been done using a function
 - use className in first tag to name that container
 - the ending tag will have /
 - almost everything is done in tags
+- always need a render
+- imrc and rfmcare shortcuts to type the react statement <3>
+- for arrow function everything in brackets is paramaters, and everything after arrow is the body of the function
+- ul tag is how we create an array of unordered elements, we then populate the ul with li tags(list items)
+- we can have code blocks within tags
+- 'a' tag is to insert a hyper link
+- href is to insert the hyperlink into the 'a' tag
+- NOTE 'item' and 'index' in the .map block are part of the map method, and they paramters in the arrow function; map methods need the arrow function to tranlate the array elements to the new array
+- item is first param it represent every element in menuitems.js, and since index is second it represents the current element being processed in array(this is an optional param)
+- apparently would have been better to use for each loop instead of .map but oh well
+- Note we are returning li's to ul tags when we are doing the map function
+- every list item needs key so theyll be able to be accessed
+
+Questions: 
+Q: WHATS A RENDER
+Q: HAVE TO RETURN SOMETHING TO RENDER; DONT KNOW WHAT YET
+Q: DONT KNOW WHY WERE PUTTING REACT MIGHT HAVE TO CHANGE THAT TO AN IMAGE OF OURS<11>
+Q: SEARCH WHAT MAP IS
+    A: .map is method array.prototype and it create a new array using the elements of the array that calling it and what ever you want to do to those elements
+    - it acts as a loop on the elements till we have accessed the last element in array that called it
 */
+}</div>
+
+export default Navbar
