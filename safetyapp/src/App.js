42abbbc71223fbe00b5b4e72376485cc352ee528
TobiOnabolu <toby.onabolu@gmail.com>
2021-05-21 19:09:22 -0400

    small

--- safetyapp/src/App.js
+++ safetyapp/src/App.js
@@ -9,11 +9,9 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Navbar/>
-      
-      <Welcomepage /> */}
-      
-      <CrimePage />
+      <Navbar/>      
+     
+
     </div>
 
   );
