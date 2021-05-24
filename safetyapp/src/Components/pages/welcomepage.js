cc173632b9ebe63846436c6e1641d62ca1a95d8f
kendzisah <dzisahken10@gmail.com>
2021-05-23 21:38:43 -0400

    Fixed positioning on welcome page

--- safetyapp/src/Components/pages/welcomepage.js
+++ safetyapp/src/Components/pages/welcomepage.js
@@ -1,36 +1,46 @@
-import './welcomepage.css';
-import React from 'react';
-import { Button } from 'reactstrap';
-
+import "./welcomepage.css";
+import React from "react";
+import { Button } from "reactstrap";
 
 function Welcomepage() {
-    return (
-        <div className="container-1">
-            <div className="header-logo">
-                <i className="fas fa-peace spin icon-size"></i>
-                <span className="logo-text"> SAFETY APP</span>
-            </div>
+  return (
+    <div className="container-1">
 
-            <section className="statement left" >
-                <h3>HOW TO PROTECT YOURSELF AND FAMILY</h3>
-                <p>Learning about a city or area ahead of time will allow
-                 you to make well guided decisions on whether you 
-                should travel or enter an area ahead of time. 
-                Staying informed is a key skill to maintaining the 
-                safety and happiness of your family and friends.</p>
-            </section>
+      <div className="header-logo">
+        <i className="fas fa-peace spin icon-size"></i>
+        <span className="logo-text"> SAFETY APP</span>
+      </div>
 
-            <section className="statement right" >
-                <h3>SAFETY, FREEDOM, PEACE, LONGEVITY</h3>
-                <p>We aim to provide people with the means and
-                 tools to enter a state of being free from danger
-                  or threat.</p>
-            </section>
+      <div className="statement-wrapper">
+        <section className="statement">
+          <h3>HOW TO PROTECT YOURSELF AND FAMILY</h3>
+          <p>
+            Learning about a city or area ahead of time will allow you to make
+            well guided decisions on whether you should travel or enter an area
+            ahead of time. Staying informed is a key skill to maintaining the
+            safety and happiness of your family and friends.
+          </p>
+        </section>
 
-            <img src="./images/temp_skyline.svg" alt="A city skyline." className="skyline-img" />
-            <Button  className="start-btn" size="lg">START NOW</Button>
-        </div>
-    )
+        <section className="statement">
+          <h3>SAFETY, FREEDOM, PEACE, LONGEVITY</h3>
+          <p>
+            We aim to provide people with the means and tools to enter a state
+            of being free from danger or threat.
+          </p>
+        </section>
+      </div>
+
+      <img
+        src="./images/temp_skyline.svg"
+        alt="A city skyline."
+        className="skyline-img"
+      />
+      <Button className="start-btn" size="lg">
+        START NOW
+      </Button>
+    </div>
+  );
 }
 
 export default Welcomepage;
